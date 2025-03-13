1dcc7336de7adb98f4628a9ec1f0633fd259a9ca
Boris Nikiforov <b.nikiforov@samsung.com>
2025-03-13 16:36:55 +0500

    images slide on button

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -17,19 +17,23 @@ const Carousel = ({ service }) => {
 
   return (
     <div className='service-carousel-container'>
-      <div className="carousel">
-        <div className="image-container">
-          <img src={images[currentIndex]} alt="" />
-        </div>
-        <div class="carousel-controls-container">
-          <div className="indicators">
-            {images.map((image, index) => (
-              <div className={`indicator ${currentIndex === index ? "active" : ""}`}></div>
-            ))}
+      <div className="carousel" style={{
+        transform: `translateX(-${currentIndex * 100}%)`
+      }}>
+        {images.map((image, index) => (
+          <div className="image-container">
+            <img className={``} src={image} alt="" />
           </div>
-          <button className="button prev" onClick={prevImage}><img src="/images/arrow-prev.png" alt="placeholder" className="placeholder" /></button>
-          <button className="button next" onClick={nextImage}><img src="/images/arrow-next.png" alt="placeholder" className="placeholder" /></button>
+        ))}
+      </div>
+      <div class="carousel-controls-container">
+        <div className="indicators">
+          {images.map((_, index) => (
+            <div className={`indicator ${currentIndex === index ? "active" : ""}`}></div>
+          ))}
         </div>
+        <button className="button prev" onClick={prevImage}><img src="/images/arrow-prev.png" alt="placeholder" className="placeholder" /></button>
+        <button className="button next" onClick={nextImage}><img src="/images/arrow-next.png" alt="placeholder" className="placeholder" /></button>
       </div>
     </div>
   );
